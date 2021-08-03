728ddba0456c24761b40c00ee3432f5bc48a910b
jukimura <juliazinha.kimura@gmail.com>
2021-08-03 19:47:01 -0300

    inicio layout chat

--- lib/screens/calendar/components/body.dart
+++ lib/screens/calendar/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:standbyme_tcc/screens/calendar/calendar_screen.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:standbyme_tcc/constants.dart';
 
 class Body extends StatelessWidget {
   @override
