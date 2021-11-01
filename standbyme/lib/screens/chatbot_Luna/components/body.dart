7a2ee008dc883731a9a21b886fc5d9831eaf2919
jukimura <juliazinha.kimura@gmail.com>
2021-11-01 16:06:26 -0300

    chatbot luna

--- standbyme/lib/screens/chatbot_Luna/components/body.dart
+++ standbyme/lib/screens/chatbot_Luna/components/body.dart
@@ -1,4 +1,143 @@
-import 'dart:ui';
+import 'package:standbyme_tcc/screens/chatbot_Luna/components/chat_message.dart';
+import 'package:standbyme_tcc/screens/chatbot_Luna/components/chat_message_list_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  final _messageList = <ChatMessage>[];
+  final _controllerText = new TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controllerText.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('Luna'),
+      ),
+      body: Column(
+        children: <Widget>[
+          _buildList(),
+          Divider(height: 1.0),
+          _buildUserInput(),
+        ],
+      ),
+    );
+  }
+
+  // Cria a lista de mensagens (de baixo para cima)
+  Widget _buildList() {
+    return Flexible(
+      child: ListView.builder(
+        padding: EdgeInsets.all(8.0),
+        reverse: true,
+        itemBuilder: (_, int index) =>
+            ChatMessageListItem(chatMessage: _messageList[index]),
+        itemCount: _messageList.length,
+      ),
+    );
+  }
+
+  // Envia uma mensagem com o padrão a direita
+  void _sendMessage({String text}) {
+    _controllerText.clear();
+    _addMessage(name: 'Você', text: text, type: ChatMessageType.sent);
+  }
+
+  // Adiciona uma mensagem na lista de mensagens
+  void _addMessage({String name, String text, ChatMessageType type}) {
+    var message = ChatMessage(text: text, name: name, type: type);
+    setState(() {
+      _messageList.insert(0, message);
+    });
+
+    if (type == ChatMessageType.sent) {
+      // Envia a mensagem para o chatbot e aguarda sua resposta
+      _dialogFlowRequest(query: message.text);
+    }
+  }
+
+  // Método incompleto ainda
+  Future _dialogFlowRequest({String query}) async {
+    _addMessage(
+        name: 'Professor',
+        text: 'Escrevendo...',
+        type: ChatMessageType.received);
+
+    // Faz a autenticação com o serviço, envia a mensagem e recebe uma resposta da Intent
+    AuthGoogle authGoogle =
+        await AuthGoogle(fileJson: "assets/chatbotluna.json").build();
+    Dialogflow dialogflow =
+        Dialogflow(authGoogle: authGoogle, language: "pt-BR");
+    AIResponse response = await dialogflow.detectIntent(query);
+
+    // remove a mensagem temporária
+    setState(() {
+      _messageList.removeAt(0);
+    });
+
+    // adiciona a mensagem com a resposta do DialogFlow
+    _addMessage(
+        name: 'Luna',
+        text: response.getMessage() ?? '',
+        type: ChatMessageType.received);
+  }
+
+  // Campo para escrever a mensagem
+  Widget _buildTextField() {
+    return new Flexible(
+      child: new TextField(
+        controller: _controllerText,
+        decoration: new InputDecoration.collapsed(
+          hintText: "Enviar mensagem",
+        ),
+      ),
+    );
+  }
+
+  // Botão para enviar a mensagem
+  Widget _buildSendButton() {
+    return new Container(
+      margin: new EdgeInsets.only(left: 8.0),
+      child: new IconButton(
+          icon: new Icon(Icons.send, color: Theme.of(context).accentColor),
+          onPressed: () {
+            if (_controllerText.text.isNotEmpty) {
+              _sendMessage(text: _controllerText.text);
+            }
+          }),
+    );
+  }
+
+  // Monta uma linha com o campo de text e o botão de enviao
+  Widget _buildUserInput() {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: new Row(
+        children: <Widget>[
+          _buildTextField(),
+          _buildSendButton(),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+/*import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:standbyme_tcc/constants.dart';
@@ -8,10 +147,11 @@ import 'package:bubble/bubble.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
 
 class Body extends StatefulWidget {
+  /*
   Body({Key key, this.title}) : super(key: key);
 
   final String title;
-
+*/
   @override
   _BodyState createState() => _BodyState();
 }
@@ -59,8 +199,8 @@ class _BodyState extends State<Body> {
                   )),
             ),
             Flexible(
-                child: ListView.builder(
-                    ),),
+              child: ListView.builder(),
+            ),
             SizedBox(
               height: 30,
             ),
@@ -178,3 +318,4 @@ class _BodyState extends State<Body> {
     );
   }
 }
+*/
